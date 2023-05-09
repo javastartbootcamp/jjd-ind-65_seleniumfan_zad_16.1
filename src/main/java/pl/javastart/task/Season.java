d5a81eabf3953d04ba5ecd64c0e10e0016c5bb43
Tomasz Iwanicki <tiko35@o2.pl>
2023-05-09 21:51:28 +0200

    solution

--- src/main/java/pl/javastart/task/Season.java
+++ src/main/java/pl/javastart/task/Season.java
@@ -1,5 +1,38 @@
 package pl.javastart.task;
 
+import java.util.Arrays;
+
 public enum Season {
-    //uzupełnij
+    SPRING("Wiosna", new String[]{"marzec", "kwiecień", "maj"}),
+    SUMMER("Lato", new String[]{"czerwiec", "lipiec", "sierpień"}),
+    AUTUMN("Jesień", new String[]{"wrzesień", "październik", "listopad"}),
+    WINTER("Zima", new String[]{"grudzień", "styczeń", "luty"});
+
+    private final String plName;
+    private final String[] months;
+
+    Season(String plName, String[] months) {
+        this.plName = plName;
+        this.months = months;
+    }
+
+    public String getPlName() {
+        return plName;
+    }
+
+    public String[] getMonths() {
+        return months;
+    }
+
+    @Override
+    public String toString() {
+        return plName;
+    }
+
+    public static Season parseToEnum(String seasonPlName) {
+        return Arrays.stream(Season.values())
+                .filter(season -> season.getPlName().equalsIgnoreCase(seasonPlName))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Brak enuma o takiej nazwie pory roku"));
+    }
 }
--- src/main/java/pl/javastart/task/SeasonManager.java
+++ src/main/java/pl/javastart/task/SeasonManager.java
@@ -1,12 +1,17 @@
 package pl.javastart.task;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SeasonManager {
-
-    // uzupełnij metodę - do wczytywania danych użyj przekazany Scanner
     public void run(Scanner scanner) {
-
+        System.out.println("Podaj porę roku");
+        Season[] seasons = Season.values();
+        for (Season season : seasons) {
+            System.out.println(season.getPlName());
+        }
+        String inputString = scanner.nextLine();
+        Season season = Season.parseToEnum(inputString);
+        System.out.println("W tej porze roku są następujące miesiące:\n" + Arrays.toString(season.getMonths()));
     }
-
 }
